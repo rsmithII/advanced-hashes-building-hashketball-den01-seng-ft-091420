623d9cb0f695d151de393dd386f01ef00662ae6c
Rodney Smith II <r_smith3@hotmail.com>
2020-09-05 01:04:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -59,7 +59,7 @@ def game_hash {
 :away => {
   team_name: "Charlotte Hornets",
   colors: ["Turquoise", "Purple"],
-  players:[
+  players:
      {player_name: "Jeff Adrien",
     number: 4,
     shoe: 18, 
