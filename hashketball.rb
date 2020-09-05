3d91e78d34b55b6e7c76588b3cb52eb8a23fe3e7
Rodney Smith II <r_smith3@hotmail.com>
2020-09-05 00:47:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ game_hash {
   team_name: "Brooklyn Nets",
   colors: "Black", "White",
   players: [
-    {player_name: "Alan Anderson"
+    {player_name: "Alan Anderson",
     number: 0,
     shoe: 16,
     points: 22,
@@ -13,15 +13,25 @@ game_hash {
     blocks: 1,
     slam_dunks: 1
     },
-    {player_name: 
-    number:
-    shoe:
-    points:
-    rebounds:
-    assists:
-    steals:
-    blocks:
-    slam_dunks:
+    {player_name: "Reggie Evans",
+    number: 30, 
+    shoe: 14,
+    points: 12,
+    rebounds: 12,
+    assists: 12,
+    steals: 12,
+    blocks: 12,
+    slam_dunks: 7
+    }, 
+     {player_name: "Brook Lopez",
+    number: 11,
+    shoe: 17,
+    points: 17, 
+    rebounds: 19,
+    assists: 10, 
+    steals: 3,
+    blocks: 1,
+    slam_dunks: 15
     }
     ]
 },
