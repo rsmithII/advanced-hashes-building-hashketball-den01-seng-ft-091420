49078bd32a763340f56e1d682cf727f1600186f3
Rodney Smith II <r_smith3@hotmail.com>
2020-09-05 00:41:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,18 @@
-# Write your code here!
+game_hash {
+:home => {
+  team_name: "Brooklyn Nets"
+  colors: "Black, White"
+  players: [
+    player_name: 
+    number:
+    shoe:
+    points:
+    rebounds:
+    assists:
+    steals:
+    blocks:
+    slam_dunks:
+    ]
+},
+:away => {
+}
